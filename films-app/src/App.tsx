5cd76cfb65e775f8e3e2893e7d52887e53e40d86
Carlos Vargas <varlopecar@gmail.com>
2023-10-31 15:30:04 +0100

    chore: search optimized

--- films-app/src/App.tsx
+++ films-app/src/App.tsx
@@ -1,27 +1,36 @@
-import { MouseEvent, ChangeEvent } from 'react';
+import { MouseEvent, ChangeEvent, useState } from 'react';
 import { Movies } from './components/MovieList';
 import useMovies from './hooks/useMovies';
 import useSearch from './hooks/useSearch';
 
 function App() {
-  const { search, setSearch, error : searchError } = useSearch();
-  const { movies, searchMovies, error, loading } = useMovies({ search });
+  const { search, setSearch, error: searchError } = useSearch();
+  const [sort, setSort] = useState(false);
+  const { movies, searchMovies, error, loading } = useMovies({ search, sort });
 
   const handleSubmit = (event: MouseEvent<HTMLFormElement>) => {
     event.preventDefault();
-    searchMovies();
+    searchMovies({ search });
   }
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   }
- 
+
+  const handleSort = () => {
+    setSort(!sort);
+  }
+
   return (
     <div>
       <header>
         <h1>Film searcher</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input name='query' type="text" className="input" placeholder="Spiderman, Ironman, etc" value={search} onChange={handleChange} />
+          <div>
+            <label htmlFor="sort">Sort by year</label>
+            <input type="checkbox" name="sort" id="sort" checked={sort} onChange={handleSort} />
+          </div>
           <button className="button">Search</button>
         </form>
         {searchError ? <p style={{ color: 'red' }}>{searchError}</p> : null}
--- films-app/src/hooks/useMovies.ts
+++ films-app/src/hooks/useMovies.ts
@@ -1,35 +1,45 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useMemo } from "react";
 import { getMovies } from "../services/getMovies";
 import { Movie } from "../models/Movie";
 
 interface useMoviesProps {
   search: string;
+  sort: boolean;
 }
 
-const useMovies = ({ search }: useMoviesProps) => {
+const useMovies = ({ search, sort }: useMoviesProps) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
   const previousSearch = useRef<string>(search);
 
-  const searchMovies = async () => {
-    if (search === previousSearch.current) return;
-    try {
-      setLoading(true);
-      const movies = await getMovies(search);
-      previousSearch.current = search;
-      setMovies(movies);
-      setLoading(false);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setError(error);
+  const searchMovies = useMemo(() => {
+    return async ({ search }: { search: string }) => {
+      if (search === previousSearch.current) return;
+      try {
+        setLoading(true);
+        const movies = await getMovies(search);
+        previousSearch.current = search;
+        setMovies(movies);
+        setLoading(false);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setError(error);
+        }
+        setLoading(false);
       }
-      setLoading(false);
-    }
-  };
+    };
+  }, []);
+
+  const sortedMovies = useMemo(() => {
+    console.log("sort");
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [movies, sort]);
 
   return {
-    movies,
+    movies: sortedMovies,
     searchMovies,
     error,
     loading,
